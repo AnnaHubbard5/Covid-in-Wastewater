20e61798dd09b0b1338fa84bf9748eb148e14281
Anna Hubbard <annahubbard567@gmail.com>
2023-02-18 12:47:59 -0800

    About Page draft 1

--- FrontEnd/src/views/AboutView.vue
+++ FrontEnd/src/views/AboutView.vue
@@ -1,8 +1,23 @@
 <template>
-  <h1>Why does this matter?</h1>
-  <p>Covid is getting worse and worse</p>
 
+  <h1>Why does this matter?</h1>
   <p>
-    The amount of SARS COV-2 in waste water can be used to predict Covid cases.
+    Three years ago, SCU sent students home from campus with an expected return of two weeks. The return date soon changed to 
+    one month and then 6 months and then 1 year. Students were not able to fully return to class in-person until over 1 year 
+    after they were sent home. It is clear that COVID is not going away anytime soon and we need to learn how to live with it. 
   </p>
+
+  <h1>What does Water Watcher COVID-19 do?</h1>
+  <p>
+    Wastewater plants can now detect for COVID in your waste. With Water Watcher COVID-19, you can sign up for email 
+    notifications that notify you of the COVID risk rate in your county.
+  </p>
+
+  <h1>How did we do it?</h1>
+  <p>
+    We used a dataset from the CDC that includes information about the sample location, date, percentile, and more. We used 
+    percentile as the metric to determine risk rate in a given county.
+  </p>
+    
+  
 </template>
