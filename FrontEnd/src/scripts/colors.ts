3ff68f0e6c7c291a324e1d3dc6a31711968488e0
Chris Carrick <36552610+cjcarrick@users.noreply.github.com>
2023-02-19 00:47:41 -0800

    create colors.ts

---
+++ FrontEnd/src/scripts/colors.ts
@@ -0,0 +1,17 @@
+const colors = {
+  red: '#bf616a',
+  orange: '#d08770',
+  yellow: '#ebcb8b',
+  green: '#a3be8c',
+  blue: '#81a1c1',
+  purple: '#b48ead',
+  black: '#2e3440',
+  darkest: '#3b4252',
+  darker: '#434c5e',
+  dark: '#4c566a',
+  light: '#d8dee9',
+  lighter: '#e5e9f0',
+  lightest: '#eceff4',
+} as const
+
+export default colors
